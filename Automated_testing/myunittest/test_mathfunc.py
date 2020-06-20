c154e26ad9563a5cc691f3bacf14106b5e10938b
hyzllg <16621381003@163.com>
2020-06-20 22:34:45 +0800

    更新

--- Automated_testing/myunittest/test_mathfunc.py
+++ Automated_testing/myunittest/test_mathfunc.py
@@ -26,7 +26,29 @@
 #
 # if __name__ == '__main__':
 #     unittest.main()
+import time
+import unittest
 
+from selenium import webdriver
+
+class TestMathCase(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+    def tearDown(self):
+        time.sleep(5)
+        self.driver.quit()
+    def test_Case_01(self):
+        url = 'http://39.98.138.157/shopxo/index.php?s=/index/user/logininfo.html'
+        self.driver.get(url)
+        self.driver.find_element_by_name("accounts").send_keys('hyzllg')
+        self.driver.find_element_by_name("pwd").send_keys('1234567890')
+        self.driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div[2]/form/div[3]/button").click()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 
