7e6d7ecb7baba93b177252eedebe29e33efc6a20
hyzllg <16621381003@163.com>
2020-06-21 17:28:36 +0800

    更新

--- Automated_testing/myunittest/test_mathfunc.py
+++ Automated_testing/myunittest/test_mathfunc.py
@@ -35,16 +35,26 @@ class TestMathCase(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
-    def tearDown(self):
-        time.sleep(5)
-        self.driver.quit()
-    def test_Case_01(self):
         url = 'http://39.98.138.157/shopxo/index.php?s=/index/user/logininfo.html'
         self.driver.get(url)
         self.driver.find_element_by_name("accounts").send_keys('hyzllg')
         self.driver.find_element_by_name("pwd").send_keys('1234567890')
         self.driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div[2]/form/div[3]/button").click()
-
+    def tearDown(self):
+        time.sleep(5)
+        self.driver.quit()
+    def test_Case_01(self):
+        self.driver.find_element_by_xpath('//*[@id="doc-topbar-collapse"]/ul/li[1]/a').click()
+        self.driver.find_element_by_xpath('//*[@id="floor1"]/div[2]/div[2]/div[1]/a/img').click()
+        current_window = self.driver.current_window_handle  # 获取当前窗口handle name
+        all_window = self.driver.window_handles
+        for window in all_window:
+            if window != current_window:
+                self.driver.switch_to.window(window)
+        current_window = self.driver.current_window_handle  # 获取当前窗口handle name
+        self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/div[2]/div[3]/div[2]/div/button').click()
+        self.driver.find_element_by_xpath('/html/body/div[4]/div/div[1]/div[2]/button').click()
+        self.driver.find_element_by_xpath('/html/body/div[4]/div/div[5]/div/form/div/button').click()
 
 
 if __name__ == '__main__':
