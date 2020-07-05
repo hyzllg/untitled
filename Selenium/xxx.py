9958aa449d1775e3734aa2cd2ad311307c0c18f0
hyzllg <16621381003@163.com>
2020-07-05 21:02:07 +0800

    更新

--- Selenium/xxx.py
+++ Selenium/xxx.py
@@ -1,39 +1,31 @@
-from selenium import webdriver
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.keys import Keys
 from time import sleep
+from selenium import webdriver
+
+url = "http://www.126.com"
 driver = webdriver.Chrome()
-#隐式等待10秒
+driver.get(url)
 driver.implicitly_wait(10)
-driver.get('http://www.baidu.com')
-#获得百度窗口句柄
-search_windows = driver.current_window_handle
-a = driver.find_element_by_xpath('//a[@name="tj_briicon"]')
-sleep(1)
-#悬停操作
-ActionChains(driver).move_to_element(a).perform()
-sleep(1)
-b = driver.find_element_by_xpath('//img[@src="https://dss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/topnav/yinyue@2x-c18adacacb.png"]')
-b.click()
-sleep(1)
-
-#切换窗口
-#获得当前所有打开的窗口句柄
+current_frame = driver.find_element_by_xpath('//iframe[@scrolling="no"]')
+driver.switch_to.frame(current_frame)
+driver.find_element_by_xpath('//input[@data-loginname="loginEmail"]').send_keys("hyzllg")
+driver.find_element_by_xpath('//input[@class="j-inputtext dlpwd"]').send_keys("123456789")
+driver.find_element_by_xpath('//input[@id="un-login"]').click()
+driver.switch_to.default_content()
+driver.find_element_by_xpath('//a[@class="f-float-left"]').click()
+current_windows = driver.current_window_handle
+print(current_windows)
 all_handles = driver.window_handles
-
-#进入音乐窗口
+print(all_handles)
 for handle in all_handles:
-    if handle != search_windows:
+    if handle != current_windows:
         driver.switch_to.window(handle)
-        driver.find_element_by_xpath('//input[@id="kw"]').send_keys("月光")
-        driver.find_element_by_xpath('//input[@id="kw"]').send_keys(Keys.ENTER)
-        sleep(1)
+        driver.find_element_by_xpath('//div[@class="custom-checkbox service"]/span').click()
+        driver.find_element_by_xpath('//input[@class="username"]').send_keys("hyzllg")
+        driver.find_element_by_xpath('//input[@class="password"]').send_keys("hyzllg33123456")
+        driver.find_element_by_xpath('//input[@class="phone"]').send_keys("16635468545")
+        driver.find_element_by_xpath('//a[@class="j-register"]').click()
+        sleep(10)
         driver.close()
-driver.switch_to.window(search_windows)
-driver.title
-driver.quit()
+driver.switch_to.window(current_windows)
 
-
-
-
-
+sleep(2)
