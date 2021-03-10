895b9b3bc06f0b17cf01ee725dc3ddf86e8c4503
hyzllg <16621381003@163.com>
2021-03-10 21:30:07 +0800

    提交

--- testcase/test_regression_0312.py
+++ testcase/test_regression_0312.py
@@ -1,5 +1,7 @@
+import os
+
 import pytest
-import Automated_testing.Repay as repay
+
 import Collect
 
 
@@ -42,3 +44,4 @@ class Test_repay_regression:
 
 if __name__ == '__main__':
     pytest.main(['test_regression_0312.py::Test_repay_regression'])
+    os.system('allure generate ./temp -o ./report --clean')
