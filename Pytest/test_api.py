14002417afc96851d90782e570157f5b29607ece
hyzllg <16621381003@163.com>
2021-06-17 21:54:18 +0800

    gengxin

--- Pytest/test_api.py
+++ Pytest/test_api.py
@@ -3,7 +3,7 @@ import requests
 import xlrd
 import json
 
-book = xlrd.open_workbook('./1111.xlsx')
+book = xlrd.open_workbook('360投保资料接口自动化测试用例.xlsx')
 sheet1 = book.sheets()[0]
 # print(sheet1.nrows)
 datas_list = []
@@ -33,9 +33,41 @@ class Testinsureinfo:
     def test01(self):
         data = get_func(datas_list[0])
         assert data == '0001',"非空校验"
-
     def test02(self):
         data = get_func(datas_list[1])
+        assert data == '0001',"非空校验"
+    def test03(self):
+        data = get_func(datas_list[2])
+        assert data == '0001',"非空校验"
+    def test04(self):
+        data = get_func(datas_list[3])
+        assert data == '0001',"非空校验"
+    def test05(self):
+        data = get_func(datas_list[4])
+        assert data == '0001',"非空校验"
+    def test06(self):
+        data = get_func(datas_list[5])
+        assert data == '0001',"非空校验"
+    def test07(self):
+        data = get_func(datas_list[6])
+        assert data == '0001',"非空校验"
+    def test08(self):
+        data = get_func(datas_list[7])
+        assert data == '0001',"非空校验"
+    def test09(self):
+        data = get_func(datas_list[8])
+        assert data == '0001',"非空校验"
+    def test10(self):
+        data = get_func(datas_list[9])
+        assert data == '0001',"非空校验"
+    def test11(self):
+        data = get_func(datas_list[10])
+        assert data == '0001',"非空校验"
+    def test12(self):
+        data = get_func(datas_list[11])
+        assert data == '0001',"非空校验"
+    def test13(self):
+        data = get_func(datas_list[12])
         assert data == '0001',"非空校验"
 
 
