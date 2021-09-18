0b306c008973750e9571e5476c6358945e21d14b
hyzllg <16621381003@163.com>
2021-09-18 21:29:18 +0800

    gengxin

--- Automated_testing/aaa.py
+++ Automated_testing/aaa.py
@@ -1,16 +1,27 @@
-import xlrd
-# s=[1,2,3,4,5]
-# q=0
-# for j in s:
-#     d=j*4
-#     s[q]=d
-#     q+=1
-# print(s)
-#
-# for a,b in enumerate(s):
-#     b*=4
-#     s[a]=b
-# print(s)
-a=1
-b=2
-print(a=b)
+import requests
+import sys
+
+def response():
+    url = 'https://aip.baidubce.com/oauth/2.0/token'
+    grant_type = 'client_credentials'
+    client_id = 'LWKrUcRsztPuaerCk56f5ndU'
+    client_secret = 'rEi9812yzgtXZqHQh2MNFbpbOaNDVcbH'
+    data = {
+        "grant_type" : grant_type,
+        "client_id" : client_id,
+        "client_secret" : client_secret
+    }
+    #获取access_token
+    try:
+        requit = requests.post(url,data=data)
+        print("cookies:%s" % (requit.cookies))
+        print("headers:%s" % (requit.headers))
+        print("url:%s" % (requit.url))
+
+
+    except:
+        print("获取token错误！")
+        sys.exit()
+    return requit.json()['access_token']
+
+response()
