99c8ea5020acfdbdc339e5dfbee6c44d8685bf2e
huangyahui <16621381003@163.com>
2021-09-18 17:28:10 +0800

    gengxin

---
+++ Automated_testing/aaa.py
@@ -0,0 +1,16 @@
+import xlrd
+# s=[1,2,3,4,5]
+# q=0
+# for j in s:
+#     d=j*4
+#     s[q]=d
+#     q+=1
+# print(s)
+#
+# for a,b in enumerate(s):
+#     b*=4
+#     s[a]=b
+# print(s)
+a=1
+b=2
+print(a=b)
