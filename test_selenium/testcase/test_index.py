58ef37620191b1c30c8c4f081cf5127aa3b1486d
hyzllg <16621381003@163.com>
2021-09-20 21:18:32 +0800

    gengxin

--- test_selenium/testcase/test_index.py
+++ test_selenium/testcase/test_index.py
@@ -1,6 +1,6 @@
 import pytest
+import yaml
 from test_selenium.page.index import Index
-from test_selenium.page.index import Login
 from page.get_ApiContentCensor_token import get_apicontentcensor_token
 
 
@@ -9,22 +9,13 @@ def get_token():
     return get_apicontentcensor_token()
 
 class TestCase:
-    def test_001(self,get_token):
-        text = "我喜欢你！"
+    with open('./datas.yaml','r',encoding='utf-8') as f:
+        texts = yaml.load(f,Loader=yaml.SafeLoader)
+    @pytest.mark.parametrize("text",texts)
+    def test_001(self,get_token,text):
         res = Index().index(get_token,text)
         print(res)
         assert res == "合规"
-    def test_002(self,get_token):
-        text = "我讨厌你！"
-        res = Index().index(get_token,text)
-        print(res)
-        assert res == "合规"
-    def test_003(self,get_token):
-        text = "尼玛"
-        res = Index().index(get_token,text)
-        print(res)
-        assert res == "合规"
-
 
 if __name__ == '__main__':
     pytest.main(['-vs','test_index.py::TestCase'])
