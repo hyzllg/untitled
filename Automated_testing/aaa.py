8894f74a01dbdb25d9d315065aac58c7efcef4ae
hyzllg <16621381003@163.com>
2021-09-21 20:41:48 +0800

    gengxin

--- Automated_testing/aaa.py
+++ Automated_testing/aaa.py
@@ -1,23 +1,6 @@
 import requests
 import sys
+import yaml
+import xlrd
 
-def response():
-    url = 'https://aip.baidubce.com/oauth/2.0/token'
-    grant_type = 'client_credentials'
-    client_id = 'LWKrUcRsztPuaerCk56f5ndU'
-    client_secret = 'rEi9812yzgtXZqHQh2MNFbpbOaNDVcbH'
-    data = {
-        "grant_type" : grant_type,
-        "client_id" : client_id,
-        "client_secret" : client_secret
-    }
-    #获取access_token
-    try:
-        requit = requests.post(url,data=data)
-        access_token = requit.json()['access_token']
-    except:
-        print("获取token错误！")
-        sys.exit()
-    return access_token
-
-response()
+book = xlrd.open_workbook('./文本审核接口测试用例.xls')
