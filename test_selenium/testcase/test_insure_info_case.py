17c5dc682b8fb4088c7994c36e72a7d4a07a8be2
huangyahui <16621381003@163.com>
2021-09-26 16:56:09 +0800

    gengxin

--- test_selenium/testcase/test_insure_info_case.py
+++ test_selenium/testcase/test_insure_info_case.py
@@ -1,5 +1,6 @@
 import pytest
 from page.insure_info_api_test import rd_excle_data
+from page.insure_info_api_test import wd_excle_data
 from page.insure_info_api_test import InsureInfo_request
 
 
@@ -10,8 +11,8 @@ class Test_InsureInfo:
     @pytest.mark.parametrize("data",rd_excle_data())
     def test_case_01(self,data):
         result = InsureInfo_request(data)
-        print(result)
-        assert result["msg"] == "SUCCESS!"
+        wd_excle_data([data[0],result])
+        assert result["msg"] == "SUCCESS!" and result["data"]["code"] == "0001" and "不允许为空" in result["data"]["message"]
 
 
 if __name__ == '__main__':
