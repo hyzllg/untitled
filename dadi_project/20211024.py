34581ced903b059ef82dc6943d8a804df881dc21
huangyahui <16621381003@163.com>
2021-10-24 19:51:19 +0800

    gengxin

--- dadi_project/20211024.py
+++ dadi_project/20211024.py
@@ -4,4 +4,3 @@ file_path = './setting/Config.yaml'
 with open(file_path,encoding='utf-8') as f:
     datas = yaml.load(f,Loader=yaml.SafeLoader)
 print(datas["xshx_oracle"]["xsxb_sit_oracle"])
-print(datas["xshx_oracle"]["xsxb_sit_oracle"])
